0874096b81de95057590d0cfb827ad453c4a9a8a
Your Name <you@example.com>
2020-09-26 22:17:21 +0300

    [FIX]

---
+++ info_pos/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+# Author      : Acespritech Solutions Pvt. Ltd. (<www.acespritech.com>)
+# Copyright(c): 2012-Present Acespritech Solutions Pvt. Ltd.
+# All Rights Reserved.
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#################################################################################
+{
+    'name': 'POS INFO',
+    'category': 'Point of Sale',
+    'summary': 'INFO POS.',
+    'description': """
+This module allows user to use combo feature in restaurant
+""",
+    'author': 'Abdelraheem.',
+    'website': '',
+    'version': '13.0.2',
+    'depends': ['base', 'point_of_sale'],
+    'images': ['static/description/main_screenshot.png'],
+    "data": [
+
+    ],
+    'qweb': ['static/src/xml/pos.xml'],
+    'installable': True,
+    'auto_install': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
