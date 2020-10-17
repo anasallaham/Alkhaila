5f23b06fda9cd47d1f2e00e257c1b3ea450db52f
Your Name <you@example.com>
2020-10-17 20:53:23 +0300

    [FIX] fIX iqama

--- payroll_edition/models/iqama_renew.py
+++ payroll_edition/models/iqama_renew.py
@@ -11,21 +11,26 @@ class IqamaRenew(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
     order_number  = fields.Char(string="Order Number",readonly=True)
-
     employee_id  = fields.Many2one('hr.employee',string="Employee")
-    account_assetw  = fields.Many2one('account.asset',string="Account Asset")
-
     job_number = fields.Char(string="Job Number",)
-
     job_title = fields.Char("Job Title",)
-
     department_id = fields.Many2one('hr.department', string="Department")
-
-    iqama_end_date = fields.Date(string="Iqama End Date")
-
-
+    iqama_end_date = fields.Date(string="Old Iqama End Date " )
     iqama_newstart_date = fields.Date(string="بداية الاقامة الجديدة",required=True)
     iqama_newend_date = fields.Date(string=" نهاية الاقامة الجديدة",required=True)
+
+
+    deferred_expense_models  = fields.Many2one('account.asset',string="Deferred Expense Models")
+    deferred_expense  = fields.Many2one('account.asset',string="Deferred Expense")
+
+
+    date_payed = fields.Date(string="تاريخ الدفع")
+    amount = fields.Float(string="القيمة",required=True)
+    journal_id = fields.Many2one('account.journal', string="اليوميه")
+    account_id = fields.Many2one('account.account', string="الحساب")
+    move_id = fields.Many2one('account.move', string="القيد",readonly=True)
+    analytic_account_id = fields.Many2one('account.analytic.account', 'Analytic Account', )
+
 
     state = fields.Selection([
         ('draft', 'Draft'),
@@ -91,6 +96,13 @@ class IqamaRenew(models.Model):
              })
 
     def draft_advanced(self):
+        self.deferred_expense.set_to_draft()
+        self.deferred_expense.unlink()
+
+        self.move_id.button_draft()
+        lines = self.env['account.move.line'].search([('move_id', '=', self.move_id.id)])
+        lines.unlink()
+
         modelid = (self.env['ir.model'].search([('model', '=', 'iqama.renew')])).id
         select = "select uid from res_groups_users_rel as gs where gs.gid in (select id from res_groups as gg where name = '%s' and category_id in (select id from ir_module_category where name = '%s')   ) " % (
             'Administrator', 'Employees')
@@ -263,10 +275,82 @@ class IqamaRenew(models.Model):
                 )
 
             )
+    def return_movelines(self):
+        all_move_vals = []
+        for rec in self:
+
+            move_vals = {
+                'date': rec.date_payed,
+                'ref': rec.order_number,
+                'journal_id': rec.journal_id.id,
+                'line_ids': [
+                    (0, 0, {
+                        'name': rec.order_number,
+                        'debit': rec.amount,
+                        'credit': 0.0,
+                        'account_id': rec.account_id.id,
+                        'analytic_account_id': rec.analytic_account_id.id,
+                        'partner_id':rec.employee_id.address_home_id.id,
+                    }),
+                    (0, 0, {
+                        'name': rec.order_number,
+                        'debit': 0.0,
+                        'credit': rec.amount,
+                        'account_id': rec.journal_id.default_credit_account_id.id,
+                        'analytic_account_id': rec.analytic_account_id.id,
+                        'partner_id': rec.employee_id.address_home_id.id,
+                    }),
+                ],
+            }
+            all_move_vals.append(move_vals)
+            return all_move_vals
+
     def action_accepted(self):
         if self.env.user.has_group('account.group_account_manager'):
+
+            account_asset = self.env['account.asset']
+            res_asset = account_asset.create(
+                {
+                    'asset_type': 'expense',
+                    'state': 'draft',
+                    'name': self.deferred_expense_models.name,
+                    'original_value': self.amount,
+                    'acquisition_date': self.date_payed,
+                    'method_number': self.deferred_expense_models.method_number,
+                    'method_period': self.deferred_expense_models.method_period,
+                    'account_depreciation_id': self.deferred_expense_models.account_depreciation_id.id,
+                    'account_depreciation_expense_id': self.deferred_expense_models.account_depreciation_expense_id.id,
+                    'journal_id': self.deferred_expense_models.journal_id.id,
+                })
+            self.deferred_expense  = res_asset.id
+            self.deferred_expense.compute_depreciation_board()
+            self.deferred_expense.validate()
+
+
             self.employee_id.iqama_start_date = self.iqama_newstart_date
             self.employee_id.iqama_end_date = self.iqama_newend_date
+            AccountMove = self.env['account.move'].with_context(default_type='entry')
+            if not self.move_id:
+                moves = AccountMove.create(self.return_movelines())
+                moves.post()
+                self.move_id = moves.id
+            else:
+                self.move_id.button_draft()
+                self.move_id.date = self.date_payed
+                move_line_vals = []
+                line1 = (0, 0, {'name': self.order_number, 'debit': self.amount, 'credit': 0,
+                                'account_id': self.account_id.id,'partner_id':self.employee_id.address_home_id.id,
+                                'analytic_account_id': self.analytic_account_id.id
+                                })
+                line2 = (0, 0, {'name': self.order_number, 'debit': 0, 'credit': self.amount,
+                                'account_id': self.journal_id.default_credit_account_id.id,'partner_id':self.employee_id.address_home_id.id,
+                                'analytic_account_id': self.analytic_account_id.id
+                                })
+                move_line_vals.append(line1)
+                move_line_vals.append(line2)
+                self.move_id.line_ids = move_line_vals
+                self.move_id.post()
+
             return self.write({'state': 'accepted'})
         else:
             raise UserError(
