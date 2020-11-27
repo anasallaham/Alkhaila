c5fedeeed75798c996b4e9c489372e632a899b3b
Your Name <you@example.com>
2020-11-27 19:15:39 +0200

    FIx Time Off

--- payroll_edition/models/hr_leave.py
+++ payroll_edition/models/hr_leave.py
@@ -6,25 +6,25 @@ from odoo.exceptions import UserError
 class HrLeave(models.Model):
     _inherit = "hr.leave"
 
-    def action_approve(self):
-        res = super(HrLeave, self).action_approve()
-        if not (self.env.user.id == self.employee_id.parent_id.user_id.id):
-            raise UserError(
-                _(
-                    "صلاحية المسئول المباشر %s فقط"%(self.employee_id.parent_id.user_id.name)
-                ))
-        return res
-
-
-
-    def action_validate(self):
-        res = super(HrLeave, self).action_validate()
-        if not self.env.user.has_group('hr.group_hr_manager'):
-            raise UserError(
-                _(
-                    "لا يمكنك الموافقة صلاحية مدير الموارد البشرية فقط"
-                ))
-        return res
-
+    # def action_approve(self):
+    #     res = super(HrLeave, self).action_approve()
+    #     if not (self.env.user.id == self.employee_id.parent_id.user_id.id):
+    #         raise UserError(
+    #             _(
+    #                 "صلاحية المسئول المباشر %s فقط"%(self.employee_id.parent_id.user_id.name)
+    #             ))
+    #     return res
+    #
+    #
+    #
+    # def action_validate(self):
+    #     res = super(HrLeave, self).action_validate()
+    #     if not self.env.user.has_group('hr.group_hr_manager'):
+    #         raise UserError(
+    #             _(
+    #                 "لا يمكنك الموافقة صلاحية مدير الموارد البشرية فقط"
+    #             ))
+    #     return res
+    #
 
 
